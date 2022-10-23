67d825304a8b945e9d6040e57ea154b38c322408
pseudo-jaycie <u21477452@tuks.co.za>
2022-10-23 18:06:12 +0200

    Completed the mediator pattern

--- main.cpp
+++ main.cpp
@@ -2,22 +2,28 @@
 #include "AssociatedCountries.h"
 #include "Country.h"
 #include "Relationship.h"
+#include "CommunicationBroadcast.h"
 int main()
 {
-    AssociatedCountries *countriesAtWar = new Relationship("Countries in the war");
-    AssociatedCountries *allies = new Relationship("Allies");
-    AssociatedCountries *axis = new Relationship("Axis");
+    Communication *tele = new CommunicationBroadcast();
+    AssociatedCountries *countriesAtWar = new Relationship("Countries in the war", tele);
+    AssociatedCountries *allies = new Relationship("Allies", tele);
+    AssociatedCountries *axis = new Relationship("Axis", tele);
+    Country *germany = new Country("Germany", tele);
 
-    allies->addAssociatedCountries(new Country("England"));
-    allies->addAssociatedCountries(new Country("France"));
-    allies->addAssociatedCountries(new Country("America"));
-    allies->addAssociatedCountries(new Country("Poland"));
-    allies->addAssociatedCountries(new Country("Russia"));
+    allies->addAssociatedCountries(new Country("England", tele));
+    allies->addAssociatedCountries(new Country("France", tele));
+    allies->addAssociatedCountries(new Country("America", tele));
+    allies->addAssociatedCountries(new Country("Poland", tele));
+    allies->addAssociatedCountries(new Country("Russia", tele));
     countriesAtWar->addAssociatedCountries(allies);
-    axis->addAssociatedCountries(new Country("Germany"));
-    axis->addAssociatedCountries(new Country("Italy"));
+    axis->addAssociatedCountries(germany);
+    axis->addAssociatedCountries(new Country("Italy", tele));
     countriesAtWar->addAssociatedCountries(axis);
-    countriesAtWar->addAssociatedCountries(new Country("Switzerland"));
+    countriesAtWar->addAssociatedCountries(new Country("Switzerland", tele));
 
-    std::cout << countriesAtWar->print() << std::endl;
+    // std::cout << countriesAtWar->print();
+
+    germany->sendBroadcast(allies, "We (germany) declare war against you");
+    allies->sendBroadcast(germany, "We (allies) accept your declaration of war and respond in kind");
 }
