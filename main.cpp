4e3e23854d3cfbd3e2ab1055dab5d5a2bb5b1838
Francois Smith <u19314486@tuks.co.za>
2022-10-28 01:24:36 +0200

    Added Revolution Command Pattern

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ int main() {
     Citizens* citizens = new Citizens;
     for (int i = 0; i < 10; i++)
     {
-        if (i == 5) {
-            cout << "Oh no a group of " << citizens->getStatus() << " citizens died!" << endl;
-            citizens->die();
+        if (i == 5 || i == 8) {
+            // cout << "Oh no a group of " << citizens->getStatus() << " citizens died!" << endl;
+            // citizens->die();
+            citizens->toggleRevolution();
         }
         string status = citizens->getStatus();
         cout << "Citizens are currently: " << status << endl;
