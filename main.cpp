26e938afaaff4c164275c466e35beb5ae6fcb90d
Wilcovw <wilcovanwyk9@gmail.com>
2022-10-28 14:06:58 +0200

    updated main to split functions

--- main.cpp
+++ main.cpp
@@ -14,13 +14,7 @@
 #include "Troops.cpp"
 #include "TrainingCamp.cpp"
 
-
 int TestArea() {
-
-#include <iostream>
-using namespace std;
-int main() {
-
     Area* testArea = new Area();
 
     TroopType* testGeneral = new Generals();
@@ -35,13 +29,10 @@ int main() {
     TrainingCamp* testNavyTraining = new NavyTraining();
     TrainingCamp* testAirforceTraining = new AirforceTraining();
 
-    
     delete testGroundTraining;
     delete testNavyTraining;
     delete testAirforceTraining;
-    cout << "No error" << endl;
     delete testGeneral;
-    cout << "Success" << endl;
     delete testSoldier;
     delete testMedic;
     delete testArea;
