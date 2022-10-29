af98edb4b63d9f045d66e12723221be2365a9a21
Francois Smith <u19314486@tuks.co.za>
2022-10-29 11:32:32 +0200

    Road,Harbour,Runway merge

--- Infrastructure.h
+++ Infrastructure.h
@@ -1,3 +1,16 @@
-class Infrastructure{
+#ifndef INFASTRUCTURE_H
+#define INFASTRUCTURE_H
+#include "Area.h"
 
+class Infrastructure {
+    private:
+        double HP;
+        Area* location;
+    public:
+        Infrastructure();
+        double getHP();
+        void destroy();
+        void takeDamage(double);
 };
+
+#endif
