a4884bbc23d5a8ed63ea6f7ef7724e97e3ed8505
DianLahouter <lahouterd@gmail.com>
2022-10-31 15:47:39 +0200

    Added Memento Pattern

---
+++ WarHistory.h
@@ -0,0 +1,22 @@
+#ifndef WARHISTORY_H
+#define WARHISTORY_H
+#include <vector>
+#include "Memento.h"
+
+using namespace std;
+
+class Memento;
+
+class WarHistory
+{
+private:
+	vector<Memento*> warhistory;
+public:
+    WarHistory();
+    Memento* getMementoAt(int index);
+    Memento* getLastMemento();
+    void storeMemento(Memento* memento);
+    vector<Memento*> getEntireHistory();
+};
+
+#endif
